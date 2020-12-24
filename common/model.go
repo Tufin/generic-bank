617a77ca4e0b4817cf443bf3bf612146acf59eae
reuvenharrison <rh@tufin.com>
2020-12-24 14:12:20 +0200

    create new account from request account - fix copy constructor

--- common/model.go
+++ common/model.go
@@ -29,7 +29,7 @@ func NewAccount(account RequestAccount) *Account {
 
 	return &Account{
 		ID:       uuid.New(),
-		Name:     "",
-		LastName: "",
+		Name:     account.Name,
+		LastName: account.LastName,
 	}
 }
